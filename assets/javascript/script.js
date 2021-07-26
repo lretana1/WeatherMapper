e89dae4a262e83dc97b17a78234c07dd46497736
LuisRetana <luisretana123@gmail.com>
2021-07-26 05:28:45 -0400

    created local variables, cities and weather functions

---
+++ assets/javascript/script.js
@@ -0,0 +1,45 @@
+// Global variable declarations
+var cityList = [];
+var cityname;
+
+// local storage functions
+CityList();
+Weather();
+
+
+// This function displays the city entered by the user into the DOM
+function renderCities(){
+    $("#cityList").empty();
+    $("#cityInput").val("");
+    
+    for (i=0; i<cityList.length; i++){
+        var a = $("<a>");
+        a.addClass("list-group-item list-group-item-action list-group-item-primary city");
+        a.attr("data-name", cityList[i]);
+        a.text(cityList[i]);
+        $("#cityList").prepend(a);
+    } 
+}
+
+// This function pulls the city list array from local storage
+function initCityList() {
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+    
+    if (storedCities !== null) {
+        cityList = storedCities;
+    }
+    
+    renderCities();
+    }
+
+// This function pull the current city into local storage to display the current weather forecast on reload
+function initWeather() {
+    var storedWeather = JSON.parse(localStorage.getItem("currentCity"));
+
+    if (storedWeather !== null) {
+        cityname = storedWeather;
+
+        displayWeather();
+        displayFiveDayForecast();
+    }
+}
